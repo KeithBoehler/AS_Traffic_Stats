da101046f0bcbbfe89bb68c6e88cb8f94b7c89e8
Keith <germanbohler@gmail.com>
2022-12-20 10:31:14 -0600

    Script that sets up the enviroment and paths for custom libs

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='csv2dict_reader',
+    packages=find_packages(),
+    version='0.1.0',
+    description='My first python lib!!',
+    author='Keith Boehler',
+    license='GPLv3'
+)
