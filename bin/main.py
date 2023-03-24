c9d8d359bc516ce2f23bfa8ecc7095b2bae32ade
Keith <germanbohler@gmail.com>
2023-03-23 20:48:38 -0500

    now with linter

--- bin/main.py
+++ bin/main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-from aslib import csv2dict_reader
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
+def main():
     print('Start!')
-    path = "/home/keith/Projects/Python/AS_Counts_Stats_Commission/data/single.csv"
-    load_csv = csv2dict_reader.csv2dict(path)
-    print(load_csv)
+    path_to_csv = "/home/keith/Projects/Python/AS_Counts_Stats_Commission/data/single.csv"
     print("Done!")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+if __name__ == '__main__':
+    main()
