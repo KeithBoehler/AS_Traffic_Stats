9df89747799ed06a98eb12177f1b576f1a133c75
Keith <germanbohler@gmail.com>
2023-04-06 22:10:31 -0500

    fixed warning with dataframe and changed some paths to test dat

--- bin/main.py
+++ bin/main.py
@@ -17,7 +17,7 @@ def datetime_dataframe(loc_dataframe):
     for i in range(len(datetime_col)):
         datetime_col[i] = datetime.strptime(datetime_col[i], '%d-%b-%Y %H:%M:%S')
     new_frame = pd.DataFrame(datetime_col, columns=["Date-Time"])
-    loc_dataframe.drop(columns="Date-Time")
+    loc_dataframe.drop(["Date-Time"], 'columns')
     converted_df = pd.concat([new_frame, loc_dataframe])
     return converted_df
 
@@ -26,7 +26,7 @@ def main():
     Intent
     '''
     print('Start!')
-    path_to_csv = "/home/keith/Projects/Python/AS_Traffic_Stats/data/real.csv"
+    path_to_csv = "/home/keith/Projects/Python/AS_Counts_Stats_Commission/data/real.csv"
     raw_data_frame = pd.read_csv(path_to_csv)
     trimmed_column_data = raw_data_frame[["Date-Time","Customer","Fulfillment Order ID","Status"]]
     trimmed_column_data = datetime_dataframe(trimmed_column_data)
